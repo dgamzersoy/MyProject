47260ed76459a2cbb94c281e1dcf7ecaae54c831
Dilek Gamze Ersoy <dgamzersoy@outlook.com>
2021-03-17 16:39:20 +0300

    ürün

---
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,67 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal : IProductDal
+    {
+        public void Add(Product entity)
+        {
+            using (ShoppingContext context = new ShoppingContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Product entity)
+        {
+            using (ShoppingContext context = new ShoppingContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            using (ShoppingContext context = new ShoppingContext())
+            {
+                return context.Set<Product>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            using (ShoppingContext context = new ShoppingContext())
+            {
+                     return filter == null  
+                    ?context.Set<Product>().ToList() 
+                    :context.Set<Product>().Where(filter).ToList();
+            }
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            using (ShoppingContext context = new ShoppingContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -11,7 +11,7 @@ namespace Entities.Concrete
         public string ProductName { get; set; }
         public int CategoryId { get; set; }
     
-        public int UnitPrice { get; set; }
+        public decimal UnitPrice { get; set; }
 
         public int UnitsInStock { get; set; }
 
